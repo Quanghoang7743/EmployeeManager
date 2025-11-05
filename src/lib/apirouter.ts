9612bb045b0b3a7bce54ff4445cb1c430daa7f08
Quanghoang7743 <qhieu774@gmail.com>
2025-11-05 08:31:00 +0700

    feat: add localhost

--- src/lib/apirouter.ts
+++ src/lib/apirouter.ts
@@ -1,1 +1,3 @@
-export const API_ROUTER = 'http://192.168.1.36:3000'
+export const API_ROUTER = window.location.hostname.includes('192.168.')
+  ? 'http://192.168.1.36:3000'
+  : 'http://localhost:3000';
